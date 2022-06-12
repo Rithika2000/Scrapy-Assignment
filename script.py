56780d9d3b084c36b381a45349a70cf8bbbf3c80
Rithika2000 <57192807+Rithika2000@users.noreply.github.com>
2022-06-12 20:22:27 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,51 @@
+pip install requests
+pip install bs4
+
+import requests
+from bs4 import BeautifulSoup
+
+#This function is used for image links extraction
+def get_topic_imgs(content):
+    selection_class = '_1fQZEK' 
+    topic_imgs_tags = content.find_all('a', {'class': selection_class})
+    topic_imgs = []
+    for tag in topic_imgs_tags:
+        if tag.img:topic_imgs.append(tag.img["src"])
+    return topic_imgs
+
+
+mb_name=[]
+mb_price=[]
+mb_configuration_details=[]
+mb_images_link=[]
+
+page_num = input('enter no. of pages')
+for i in range(1,int(page_num)+1):
+    url = "https://www.flipkart.com/mobiles/pr?sid=tyy%2C4io&marketplace=FLIPKART&page=" + str(i)
+    req=requests.get(url)
+    content=BeautifulSoup(req.content,'html.parser')
+    
+    name=content.find_all('div',{"class":"_4rR01T"})
+    price=content.find_all('div',{"class":"_30jeq3 _1_WHN1"})
+    configuration_details = content.find_all('span',{"class":"_2_R_DZ"}) 
+    mb_images_link = get_topic_imgs(content)  
+    battery = content.find_all('li',{"class":"rgWa7D"})[3].text
+    
+    
+    for i in name:
+        mb_name.append(i.text)
+    for i in price:
+        mb_price.append(i.text)
+    for i in configuration_details:
+        mb_configuration_details.append(i.text)
+        
+print(len(mb_name))
+print(len(mb_price))
+print(len(mb_images_link))
+print(len(mb_configuration_details))
+
+import pandas as pd
+df=pd.DataFrame({'Product Name':mb_name,'configuration_details':mb_configuration_details,'image_links':mb_images_link,'Price':mb_price})
+
+df.to_excel(r'C:\Users\home\OneDrive\Documents\py\project.xlsx', sheet_name='Sheet_name_1')
+    
